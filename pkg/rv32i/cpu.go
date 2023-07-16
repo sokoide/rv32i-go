abe7e0443d9282a22740a0ba85749149b7f33fa7
So Koide <sokoide@gmail.cojm>
2023-07-16 23:25:43 +0900

    add: all instruction skeleton

--- pkg/rv32i/cpu.go
+++ pkg/rv32i/cpu.go
@@ -82,12 +82,30 @@ func (c *Cpu) Execute(i *Instruction) bool {
 	case OpAuipc:
 		log.Tracef("auipc: X[%d] <- PC:%x + imm:%x", i.Rd, c.PC, i.Imm)
 		c.X[i.Rd] = c.PC + i.Imm
-	case OpAddi:
-		log.Tracef("addi: rs1:%x + imm:%x -> rd:%x", i.Rs1, i.Imm, i.Rd)
-		c.X[i.Rd] = c.X[i.Rs1] + i.Imm
-	case OpAdd:
-		log.Tracef("add: rs1:%x + rs2:%x -> rd:%x", i.Rs1, i.Rs2, i.Rd)
-		c.X[i.Rd] = c.X[i.Rs1] + c.X[i.Rs2]
+	case OpJal:
+		t := c.PC + 4
+		c.PC += i.Imm
+		c.X[i.Rd] = t
+		log.Tracef("jal: PC=%x, X[%d]=%x", c.PC, i.Rd, t)
+		incrementPC = false
+	case OpJalr:
+		t := c.PC + 4
+		c.PC = (c.X[i.Rs1] + i.Imm)
+		c.X[i.Rd] = t
+		log.Tracef("jalr: PC=%x, X[%d]=%x", c.PC, i.Rd, t)
+		incrementPC = false
+	case OpBeq:
+		panic("Not implemented yet")
+	case OpBne:
+		panic("Not implemented yet")
+	case OpBlt:
+		panic("Not implemented yet")
+	case OpBge:
+		panic("Not implemented yet")
+	case OpBltu:
+		panic("Not implemented yet")
+	case OpBgeu:
+		panic("Not implemented yet")
 	case OpLb:
 		addr := c.X[i.Rs1] + i.Imm
 		log.Tracef("lb: read %x -> X[%d]", addr, i.Rd)
@@ -105,6 +123,10 @@ func (c *Cpu) Execute(i *Instruction) bool {
 		log.Tracef("lw: read %x -> X[%d]", addr, i.Rd)
 		data := c.Emu.ReadU32(addr)
 		c.X[i.Rd] = data
+	case OpLbu:
+		panic("Not implemented yet")
+	case OpLhu:
+		panic("Not implemented yet")
 	case OpSb:
 		addr := c.X[i.Rs1] + i.Imm
 		data := uint8(c.X[i.Rs2] & 0xFF)
@@ -120,21 +142,68 @@ func (c *Cpu) Execute(i *Instruction) bool {
 		data := c.X[i.Rs2]
 		log.Tracef("sw: write %x at %x", data, addr)
 		c.Emu.WriteU32(addr, data)
-	case OpJal:
-		t := c.PC + 4
-		c.PC += i.Imm
-		c.X[i.Rd] = t
-		log.Tracef("jal: PC=%x, X[%d]=%x", c.PC, i.Rd, t)
-		incrementPC = false
-	case OpJalr:
-		t := c.PC + 4
-		c.PC = (c.X[i.Rs1] + i.Imm)
-		c.X[i.Rd] = t
-		log.Tracef("jalr: PC=%x, X[%d]=%x", c.PC, i.Rd, t)
-		incrementPC = false
+	case OpAddi:
+		log.Tracef("addi: rs1:%x + imm:%x -> rd:%x", i.Rs1, i.Imm, i.Rd)
+		c.X[i.Rd] = c.X[i.Rs1] + i.Imm
+	case OpSlti:
+		panic("Not implemented yet")
+	case OpSltiu:
+		panic("Not implemented yet")
+	case OpXori:
+		panic("Not implemented yet")
+	case OpOri:
+		panic("Not implemented yet")
+	case OpAndi:
+		panic("Not implemented yet")
+	case OpSlli:
+		panic("Not implemented yet")
+	case OpSrli:
+		panic("Not implemented yet")
+	case OpSrai:
+		panic("Not implemented yet")
+	case OpAdd:
+		log.Tracef("add: rs1:%x + rs2:%x -> rd:%x", i.Rs1, i.Rs2, i.Rd)
+		c.X[i.Rd] = c.X[i.Rs1] + c.X[i.Rs2]
+	case OpSub:
+		panic("Not implemented yet")
+	case OpSll:
+		panic("Not implemented yet")
+	case OpSlt:
+		panic("Not implemented yet")
+	case OpSltu:
+		panic("Not implemented yet")
+	case OpXor:
+		panic("Not implemented yet")
+	case OpSrl:
+		panic("Not implemented yet")
+	case OpSra:
+		panic("Not implemented yet")
+	case OpOr:
+		panic("Not implemented yet")
+	case OpAnd:
+		panic("Not implemented yet")
+	case OpFence:
+		panic("Not implemented yet")
+	case OpFenceI:
+		panic("Not implemented yet")
+	case OpEcall:
+		panic("Not implemented yet")
+	case OpEbreak:
+		panic("Not implemented yet")
+	case OpCsrrw:
+		panic("Not implemented yet")
+	case OpCsrrs:
+		panic("Not implemented yet")
+	case OpCsrrc:
+		panic("Not implemented yet")
+	case OpCsrrwi:
+		panic("Not implemented yet")
+	case OpCsrrsi:
+		panic("Not implemented yet")
+	case OpCsrrci:
+		panic("Not implemented yet")
 	default:
-		// TODO: must implemente all operators
-		panic(fmt.Sprintf("Op: %s not supproted yet", op))
+		panic(fmt.Sprintf("Op: %s invalid", op))
 	}
 	return incrementPC
 }
